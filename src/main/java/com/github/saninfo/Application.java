91782c9035880eb17cb442aa712853c023f0d6f4
San <san6031@gmail.com>
2018-07-07 18:14:27 -0500

    updated

--- src/main/java/com/github/saninfo/Application.java
+++ src/main/java/com/github/saninfo/Application.java
@@ -17,8 +17,10 @@ package com.github.saninfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource({"classpath:application.properties"})
 public class Application {
 
     public static void main(String[] args) {
