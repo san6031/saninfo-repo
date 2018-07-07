1ada3fcdc44203cfaa1f29daea7a48b20c766822
San <san6031@gmail.com>
2018-07-07 17:52:55 -0500

    updated

--- src/main/java/com/github/saninfo/HomeController.java
+++ src/main/java/com/github/saninfo/HomeController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Benedikt Ritter
+ * Copyright 2018 san6031
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,43 +15,16 @@
  */
 package com.github.saninfo;
 
-import javax.validation.Valid;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
 
-    private RecordRepository repository;
-
-    @Autowired
-    public HomeController(RecordRepository repository) {
-        this.repository = repository;
-    }
-
-    @RequestMapping(method = RequestMethod.GET)
-    public String home(ModelMap model) {
-       // List<Record> records = repository.findAll();
-     //   model.addAttribute("records", records);
-       // model.addAttribute("insertRecord", new Record());
+	@RequestMapping("/")
+    public String insertData(ModelMap model) {
+        
         return "home";
-    }
-
-    @RequestMapping(method = RequestMethod.POST)
-    public String insertData(ModelMap model, 
-                             @ModelAttribute("insertRecord") @Valid Record record,
-                             BindingResult result) {
-        if (!result.hasErrors()) {
-            repository.save(record);
-        }
-        return home(model);
     }
 }
